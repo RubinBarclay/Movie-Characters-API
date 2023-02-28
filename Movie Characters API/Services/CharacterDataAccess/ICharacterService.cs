d5d2ba811a5ea3454653009cf551827e0575bf21
Jakob-mbi <jakob_mbi@msn.com>
2023-02-28 19:53:50 +0100

    fix: removing Services to work on one at a time

--- Movie Characters API/Services/CharacterDataAccess/ICharacterService.cs
+++
@@ -1,8 +0,0 @@
-﻿using Movie_Characters_API.Models;
-
-namespace Movie_Characters_API.Services.CharacterDataAccess
-{
-    public interface ICharacterService : ICrudRepository<Character,int>
-    {
-    }
-}
