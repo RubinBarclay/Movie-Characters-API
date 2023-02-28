d86a220d9ff40f223eea5ab538d486a034d2ce2c
Jakob-mbi <jakob_mbi@msn.com>
2023-02-28 18:53:14 +0100

    feat: added to builder services

--- Movie Characters API/Program.cs
+++ Movie Characters API/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Movie_Characters_API.Models;
-
+using Movie_Characters_API.Services.CharacterDataAccess;
+using Movie_Characters_API.Services.FranchiseDataAccess;
+using Movie_Characters_API.Services.MovieDataAccess;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +15,11 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<MovieDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddTransient<ICharacterService, CharacterService>();
+builder.Services.AddTransient<IMovieService, MovieService>();
+builder.Services.AddTransient<IFranchiseService, FranchiseService>();
+builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
+
 
 var app = builder.Build();
 
