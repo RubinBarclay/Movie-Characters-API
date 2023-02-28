8a9b11784ff49edc3794ffef31e2f906c1748aac
Jakob-mbi <jakob_mbi@msn.com>
2023-02-28 19:51:06 +0100

    refactor: DTOs for Franchise

---
+++ Movie Characters API/DTOs/DTOFranchiseModels/DTOCreateFranchise.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movie_Characters_API.DTOs.DTOFranchiseModels
+{
+    public class DTOCreateFranchise
+    {
+        [Required]
+        [MaxLength(200)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(2000)]
+        public string Description { get; set; }
+    }
+}
---
+++ Movie Characters API/DTOs/DTOFranchiseModels/DTOFranchise.cs
@@ -0,0 +1,19 @@
+﻿using Movie_Characters_API.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Movie_Characters_API.DTOModels.DTOFranchiseModels
+{
+    public class DTOFranchise
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(2000)]
+        public string Description { get; set; }
+
+    }
+}
